d84413c59028c279113d121192406fceb2fc96aa
micaetranquocbao <micaetranquocbao2001@gmail.com>
2022-03-19 00:10:18 +0700

    New feature: Enter name, detect ip of user.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -18,25 +19,55 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
+
   console.log("a user connected");
-console.log(socket.request.headers['x-forwarded-for'] || socket.request.socket.remoteAddress);
+  var fullname = "";
+  socket.on('send-name', (response)=>{
+    fullname = response;
+    socket.nsp.emit("send-chat-message-admin", {
+      fullname: 'FROM ADMIN:',
+      text: `User ${socket.client.id}||${fullname} connected!!!`
+    });
+  })
+
+  const sha_last_index_colon = socket.handshake.address.lastIndexOf(":");
+  if (sha_last_index_colon > -1) {
+    if (socket.handshake.address.lastIndexOf(".") > -1) {
+      socket.getIp = {
+        ipv4: socket.handshake.address.slice(sha_last_index_colon + 1),
+        ipv6: socket.handshake.address.slice(0, sha_last_index_colon),
+      };
+    } else {
+      socket.getIp = {
+        ipv6: socket.handshake.address,
+      };
+    }
+  } else {
+    socket.getIp = {
+      ipv4: socket.handshake.address,
+    };
+  }
+
   var info = {
     id: socket.client.id,
     timeSend: Date.now(),
-    // ipClient: socket.request?.,
+    ipClientV4: socket.getIp.ipv4,
+    ipClientV6: socket.getIp.ipv6,
   };
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
+    socket.broadcast.emit("send-chat-message-admin", {
+      fullname: 'ADMIN:',
+      text: `User ${socket.client.id}||${fullname} disconnected!!!`
+    });
   });
   socket.on("send-chat-message", (msg) => {
-    msg = {...info, ...msg}
-    io.emit('send-chat-message', msg);
+    msg = { ...info, ...msg };
+    io.emit("send-chat-message", msg);
     console.log("Sủa:", msg);
   });
-  setInterval(()=>{  
-    socket.broadcast.emit("phatTinHieu", 'iu iu');
-  }, 5000);
+
 });
 
 server.listen(1025, () => {
